1aced084da571a67399c660772a852f5d508a2fa
sudheerlegendyt-jpg <sudheerlegendyt@gmail.com>
2026-07-11 18:49:48 +0530

    Update Registration.js

--- server/models/Registration.js
+++ server/models/Registration.js
@@ -1,1 +1,27 @@
+const mongoose = require("mongoose");
+
+const TournamentSchema = new mongoose.Schema({
+  playerName: {
+    type: String,
+    required: true,
+  },
+  gameUID: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Tournament", TournamentSchema);
 
