492b09a27ddebcf05c805b26096160cb72be12c9
sudheerlegendyt-jpg <sudheerlegendyt@gmail.com>
2026-07-11 18:50:35 +0530

    Update Tournament.js

--- server/models/Tournament.js
+++ server/models/Tournament.js
@@ -1,1 +1,38 @@
+const mongoose = require("mongoose");
 
+const TournamentSchema = new mongoose.Schema(
+{
+    tournamentName: {
+        type: String,
+        required: true
+    },
+    game: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    entryFee: {
+        type: Number,
+        required: true
+    },
+    prizePool: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["Upcoming", "Live", "Completed"],
+        default: "Upcoming"
+    },
+    description: {
+        type: String
+    }
+},
+{
+    timestamps: true
+});
+
+module.exports = mongoose.model("Tournament", TournamentSchema);
