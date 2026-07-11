60dfc7d09b9e28dea3e6e77167767c7126fd8f76
sudheerlegendyt-jpg <sudheerlegendyt@gmail.com>
2026-07-11 22:54:09 +0530

    Update Player.js

--- server/models/Player.js
+++ server/models/Player.js
@@ -1,26 +1,43 @@
 const mongoose = require("mongoose");
 
 const PlayerSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
 
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+    name:{
+        type:String,
+        required:true
+    },
 
-  password: {
-    type: String,
-    required: true,
-  },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
 
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+    password:{
+        type:String,
+        required:true
+    },
+
+    tournament:{
+        type:String,
+        default:"No Tournament Joined"
+    },
+
+    paymentStatus:{
+        type:String,
+        default:"Pending"
+    },
+
+    notice:{
+        type:String,
+        default:"No New Notice"
+    },
+
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+
 });
 
 module.exports = mongoose.model("Player", PlayerSchema);
