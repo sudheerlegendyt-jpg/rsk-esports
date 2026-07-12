a656982493c44ad95ae64807c6e7764b1f1aff5d
sudheerlegendyt-jpg <sudheerlegendyt@gmail.com>
2026-07-12 11:15:05 +0530

    Update cloudinary.js

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -3,7 +3,7 @@ const cloudinary = require("cloudinary").v2;
 cloudinary.config({
     cloud_name: "xnx7yfbi",
     api_key: "792946512442473",
-    api_secret: "YOUR_API_SECRET"
+    api_secret: "ujYGwciKNflSQcwJ23fq5HSExSQ"
 });
 
 module.exports = cloudinary;
